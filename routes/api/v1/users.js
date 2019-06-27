09825041d97f6abbc0f6596a217f501ecd8d1d6d
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-26 21:15:29 -0600

    Gives hashed password functionality to user save in route file users.js

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,21 +2,25 @@ var express = require('express');
 var router = express.Router();
 var uuidv1 = require('uuid/v1');
 var User = require('../../../models').User;
+var bcrypt = require('bcrypt')
+const saltRounds = 9;
 var pry = require('pryjs');
 
 /* POST new user. */
 router.post('/', function(req, res, next) {
-  User.create({
-    email: req.body.email,
-    password: req.body.password,
-    api_key: uuidv1()
-  }).then(user => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(201).send(JSON.stringify({api_key: user.api_key}));
-  }).catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(error, hashed_password) {
+    User.create({
+      email: req.body.email,
+      password: hashed_password,
+      api_key: uuidv1()
+    }).then(user => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify({api_key: user.api_key}));
+    }).catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+  });
 });
 
 module.exports = router;
