a6b847caa117ee6f07cf17937dba2773e950d720
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-26 23:25:47 -0600

    Adds post session route without password validation

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../../../models').User;
-var session = require('express-session')
+var session = require('express-session');
+var bcrypt = require('bcrypt')
+
+var pry = require('pryjs');
 
 /* POST new session (Login) */
 
 router.post('/', function(req, res, next) {
-
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(user => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify({ api_key: user.api_key }))
+  }).catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send({ error });
+  });
 });
+
+module.exports = router;
