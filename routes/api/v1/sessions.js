94601b1bbcd17896cdc937555e7807c79888f3d3
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-26 22:00:09 -0600

    Creates skeleton sessions route file with empty router.post function

---
+++ routes/api/v1/sessions.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../../../models').User;
+var session = require('express-session')
+
+/* POST new session (Login) */
+
+router.post('/', function(req, res, next) {
+
+});
