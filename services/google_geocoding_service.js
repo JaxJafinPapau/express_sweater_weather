f6e584652ea58e3b4ec53276ca018ec339dbbaa4
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-30 16:08:35 -0600

    Creates google_geocoding_service which allows the service to be instantiated with 'new' and on that variable .latitude and .longitude are mthods which return those attributes when passed an address that can be parsed by google's geocoding api

--- services/google_geocoding_service.js
+++ services/google_geocoding_service.js
@@ -1,1 +1,15 @@
 const fetch = require('node-fetch')
+
+module.exports = class GoogleGeocodingService {
+  constructor(location) {
+    this.location = location
+    this.getLatLong()
+  }
+
+  async getLatLong() {
+    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.location}&key=${process.env.GOOGLE_API_KEY}`)
+    this.data = await response.json()
+    this.latitude = await this.data.results[0].geometry.location.lat
+    this.longitude = await this.data.results[0].geometry.location.lng
+  }
+}
---
+++ services/index.js
@@ -0,0 +1,5 @@
+const GoogleGeocodingService = require('./google_geocoding_service')
+
+module.exports = {
+  GoogleGeocodingService: GoogleGeocodingService
+}
