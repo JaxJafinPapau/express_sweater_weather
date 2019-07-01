8facc3ea86e071481fa4c71a471fa8fa7e0408c4
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-30 19:20:48 -0600

    Updates darksky service to allow promise data to be parsed

--- services/dark_sky_service.js
+++ services/dark_sky_service.js
@@ -6,12 +6,13 @@ module.exports = class DarkSkyService {
   constructor(latitude, longitude) {
     this.latitude = latitude
     this.longitude = longitude
-    this.getWeather()
   }
 
   async getWeather() {
     let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${this.latitude},${this.longitude}`)
-    this.data = await response.json()
-    eval(pry.it)
+    let data = await response.json()
+    this.hourly = await data.hourly.data.slice(0, 8)
+    this.daily = await data.daily.data.slice(0, 7)
+    return this
   }
 }
