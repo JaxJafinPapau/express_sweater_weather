aeb4edbd478a4120ffa631ecaf295998e7a20acb
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-30 19:36:54 -0600

    Updates dark sky service for expected return value by project spec.

--- services/dark_sky_service.js
+++ services/dark_sky_service.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch')
 var pry = require('pryjs')
 
 module.exports = class DarkSkyService {
-  constructor(latitude, longitude) {
+  constructor(userLocation, latitude, longitude) {
+    this.location = userLocation
     this.latitude = latitude
     this.longitude = longitude
   }
@@ -11,8 +12,10 @@ module.exports = class DarkSkyService {
   async getWeather() {
     let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${this.latitude},${this.longitude}`)
     let data = await response.json()
-    this.hourly = await data.hourly.data.slice(0, 8)
-    this.daily = await data.daily.data.slice(0, 7)
+    eval(pry.it)
+    this.currently = await data.currently
+    this.hourly = await data.hourly
+    this.daily = await data.daily
     return this
   }
 }
