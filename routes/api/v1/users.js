4f0581b495e2eb4f1bce35b3abe904e98d6ca944
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-10-05 11:49:59 -0700

    Adds validation of password matching passwordConfirmation in POST user route.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -7,21 +7,28 @@ const saltRounds = 9;
 
 /* POST new user. */
 router.post('/', function(req, res, next) {
-  /* Encrypts the new password before anything else */
-  bcrypt.hash(req.body.password, saltRounds, function(error, hashed_password) {
-    User.create({
-      email: req.body.email,
-      password: hashed_password,
-      /* Generates a random api key using the uuidv1 package */
-      api_key: uuidv1()
-    }).then(user => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify({api_key: user.api_key}));
-    }).catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send({ error });
+  let password = req.body.password
+  let confirmation = req.body.passwordConfirmation
+  if(password === confirmation) {
+    /* Encrypts the new password before anything else */
+    bcrypt.hash(password, saltRounds, function(error, hashed_password) {
+      User.create({
+        email: req.body.email,
+        password: hashed_password,
+        /* Generates a random api key using the uuidv1 package */
+        api_key: uuidv1()
+      }).then(user => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify({api_key: user.api_key}));
+      }).catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error });
+      });
     });
-  });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send({error: "Password does not match password confirmation"})
+  }
 });
 
 module.exports = router;
